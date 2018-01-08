834e77522d8b0aaa5113abc702ad7cf97858a245
ssborbis <mike@biolsi.com>
2018-01-08 15:29:37 -0600

    Add files via upload

--- ContextSearch.py
+++ ContextSearch.py
@@ -36,12 +36,17 @@ def send_message(encoded_message):
 message = get_message()
 message_json = json.loads(message)
 
-if message_json['!@!@'] != "":
+if '!@!@' in message_json:
     mod_time = os.path.getmtime(message_json['!@!@'])
     send_message(encode_message({"last_mod": time.ctime(mod_time), "base64": ""}))
     sys.exit(0)
 
 with open(message_json['path'], "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read())
+	
+with open('test.txt', "w") as logfile:
+    logfile.write(message_json['path'])
+    logfile.write(encoded_string)
 
+mod_time = os.path.getmtime(message_json['path'])
 send_message(encode_message({"last_mod": time.ctime(mod_time), "base64": encoded_string}))
