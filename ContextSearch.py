c4ed545999c3aa1826644981ae8afb554a67f228
ssborbis <ssborbis@gmail.com>
2022-02-11 23:12:43 -0600

    migrating to app launcher

--- ContextSearch.py
+++ ContextSearch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -u
+#!/usr/bin/python3 -u
 
 # Note that running python with the `-u` flag is required on Windows,
 # in order to ensure that stdin and stdout are opened in binary, rather
@@ -7,40 +7,41 @@
 import json
 import sys
 import struct
-import base64
 import os
-import time
 
 # Read a message from stdin and decode it.
 def get_message():
-    raw_length = sys.stdin.read(4)
+    raw_length = sys.stdin.buffer.read(4)
+
     if not raw_length:
         sys.exit(0)
-    message_length = struct.unpack('@I', raw_length)[0]
-    message = sys.stdin.read(message_length)
+    message_length = struct.unpack('=I', raw_length)[0]
+    message = sys.stdin.buffer.read(message_length).decode("utf-8")
     return json.loads(message)
 
 # Encode a message for transmission, given its content.
 def encode_message(message_content):
-    encoded_content = json.dumps(message_content)
-    encoded_length = struct.pack('@I', len(encoded_content))
-    return {'length': encoded_length, 'content': encoded_content}
+    encoded_content = json.dumps(message_content).encode("utf-8")
+    encoded_length = struct.pack('=I', len(encoded_content))
+    #  use struct.pack("10s", bytes), to pack a string of the length of 10 characters
+    return {'length': encoded_length, 'content': struct.pack(str(len(encoded_content))+"s",encoded_content)}
 
 # Send an encoded message to stdout.
 def send_message(encoded_message):
-    sys.stdout.write(encoded_message['length'])
-    sys.stdout.write(encoded_message['content'])
-    sys.stdout.flush()
+    sys.stdout.buffer.write(encoded_message['length'])
+    sys.stdout.buffer.write(encoded_message['content'])
+    sys.stdout.buffer.flush()
 
 message = get_message()
-message_json = json.loads(message)
 
-if '!@!@' in message_json:
-    mod_time = os.path.getmtime(message_json['!@!@'])
-    send_message(encode_message({"last_mod": time.ctime(mod_time)}))
+if not message.get("verify") is None:
+    send_message(encode_message(True))
     sys.exit(0)
-else:
-    mod_time = os.path.getmtime(message_json['path'])
-    with open(message_json['path'], "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-        send_message(encode_message({"last_mod": time.ctime(mod_time), "base64": encoded_string}))
+
+if not message.get("path") is None:
+    os.system(message["path"])
+    send_message(encode_message(True))
+    sys.exit(0)
+
+send_message(encode_message(False))
+sys.exit(1)
